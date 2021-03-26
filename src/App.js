63a7e6ce3527b8368c02186209d3074ac4ad7632
LavaCode <kasper.zegel@gmail.com>
2021-03-26 14:06:57 +0100

    Removed 'haal data op'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { cloneElement, useState } from 'react';
 import SearchBar from './components/searchBar/SearchBar';
 import TabBarMenu from './components/tabBarMenu/TabBarMenu';
 import MetricSlider from './components/metricSlider/MetricSlider';
+import axios from 'axios';
 import './App.css';
 
-const apiKey = '9b585d8e9d62731b761fca47daf7c164';
+const apiKey = 'f47d4f545dbec124986ae15ba8de1f30';
 
 function App() {
+  const [location, setLocation] = useState(null);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchData() {
+    try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=utrecht,nl&appid=${apiKey}&lang=nl`);
+      setWeatherData(result.data);
+      console.log(result.data);
+      console.log(weatherData.name);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <>
+
       <div className="weather-container">
 
         {/*HEADER -------------------- */}
         <div className="weather-header">
-          <SearchBar/>
+          <SearchBar setLocation={setLocation}/>
 
           <span className="location-details">
-            <h2>Bewolkt</h2>
-            <h3> </h3>
-            <h1>14 &deg;</h1>
+            { weatherData && 
+              <>
+                <h2>{weatherData.weather[0].description}</h2>
+                <h3>{weatherData.name}</h3>
+                <h1>{weatherData.main.temp} &deg;</h1>
+              </>
+            }
 
-            <button type="button">
+            <button type="button" onClick={fetchData}>
               Haal data op!
             </button>
           </span>
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-function SearchBar() {
+function SearchBar({ setLocation }) {
+  const [query, setQuery] = useState('');
+
+  const handleClick = () => {
+    console.log('Je hebt zoek geklikt');
+    setLocation(query);
+    console.log(query);
+    };
+  
+  const handleKeyPress = e => {
+    if(e.keyCode === 13){
+      handleClick();
+    }
+  };
+
   return (
     <span className="searchbar">
       <input
         type="text"
         name="search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown= {handleKeyPress}
         placeholder="Zoek een stad in Nederland"
       />
 
-      <button type="button">
+      <button onClick={handleClick} type="button">
         Zoek
       </button>
     </span>
